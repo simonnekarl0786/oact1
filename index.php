f23190d5150c2eeed05d16a903f5f0cac1758aef
simonnekarl0786 <43148804+simonnekarl0786@users.noreply.github.com>
2018-10-04 02:27:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://grandfurnitures.us/mainnew/1/index.php");
+header("Location: https://www.amazon.com/Quality-Durables-Co-Stretch-Charcoal/dp/B074N7KY3J/ref=sr_1_7_acs_sk_pb_1_sl_3?s=apparel&ie=UTF8&qid=1535880301&sr=1-7-acs&nodeID=7141123011&psd=1&keywords=jeans");
 die();
 ?>
